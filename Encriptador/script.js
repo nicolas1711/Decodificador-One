7a0e1e48ac7c3e956e096c59c6fa03e6824a4ffa
nicolas1711 <124020824+nicolas1711@users.noreply.github.com>
2023-01-31 02:27:31 -0300

    Decodificador-One
    Encripitador ONE alura

---
+++ Encriptador/script.js
@@ -0,0 +1,38 @@
+const inputtexto = document.querySelector(".input-texto");
+const mensagem = document.querySelector(".mensagem");
+
+function btnEncriptar() {
+   const textoEncriptado = encriptar(inputtexto.value)
+   mensagem.value = textoEncriptado
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"],["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i= 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes (matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada;
+}
+
+
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(inputtexto.value)
+    mensagem.value = textoDesencriptado
+ }
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"],["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i= 0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes (matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1] ,matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada;
+}
